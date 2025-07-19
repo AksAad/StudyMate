b5c32a47f3e78b0d9d6c8665c9fc303b07d6b494
AkshatSoni <soni.akshat925@gmail.com>
2025-07-19 09:57:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+const API_URL = "https://zenquotes.io/api/random";
+app.get("/", async(req, res) => {
+  try{
+    const response = await axios.get(API_URL);
+    const quote = response.data[0].q;
+    const author = response.data[0].a;
+    console.log(quote);
+    res.render("index.ejs",{quote,author});
+  }catch(error){
+    console.error(error);
+    res.render("index.ejs", {quote: "could not load quote" , author: ""});
+  }
 });
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
